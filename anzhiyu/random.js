ec513759bd797234046459327a995c972a0c52ad
caffeinemn <3454306652@qq.com>
2025-08-27 15:52:22 +0800

    Site updated: 2025-08-27 15:52:22

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/27/hello-world/"];function toRandomPost(){
+var posts=["2025/08/27/应该算是翻新好了吧/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
